f8700131897ba18a9712ad18692e31fe2fc3c683
Oliver Charles <oliver.g.charles@googlemail.com>
2012-01-17 22:05:21 +0000

    Added getLabelByMBID

--- Web/MusicBrainz.hs
+++ Web/MusicBrainz.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Web.MusicBrainz (getArtistByMBID, runMusicBrainz) where
+module Web.MusicBrainz
+    ( getArtistByMBID
+    , getLabelByMBID
+    , runMusicBrainz) where
 
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Reader (ReaderT, ask, runReaderT)
@@ -8,6 +11,7 @@ import Control.Monad.Trans (lift)
 import Control.Monad.Trans.Resource (transResourceT)
 import qualified Data.ByteString.Char8 as BS
 import Data.Conduit (($$), ResourceT, ResourceIO, runResourceT)
+import Data.Text (Text)
 import Data.UUID (UUID, toString)
 import Network.HTTP.Conduit (Manager, http, responseBody, newManager)
 import qualified Network.HTTP.Conduit as H
@@ -17,18 +21,23 @@ import Text.XML.Cursor (fromDocument)
 
 import Text.XML.Cursor.FromXML
 import Web.MusicBrainz.Types
-import Web.MusicBrainz.Monad
-import Web.MusicBrainz.XML
+import Web.MusicBrainz.XML ()
 
 getArtistByMBID :: UUID -> ResourceT (ReaderT Manager IO) Artist
-getArtistByMBID mbid = do
-  let req = H.def { H.path = BS.pack ("/ws/2/artist/" ++ toString mbid)
+getArtistByMBID mbid =
+  runAndParse ("/ws/2/artist/" ++ toString mbid) [ "artist" ]
+
+getLabelByMBID :: UUID -> ResourceT (ReaderT Manager IO) Label
+getLabelByMBID mbid =
+  runAndParse ("/ws/2/label/" ++ toString mbid) [ "label" ]
+
+runAndParse :: FromXML a => String -> [Text] -> ResourceT (ReaderT Manager IO) a
+runAndParse resourcePath docPath = do
+  let req = H.def { H.path = BS.pack resourcePath
                   , H.host = "musicbrainz.org" }
-  manager <- lift ask
-  res <- http req manager
+  res <- lift ask >>= http req
   document <- responseBody res $$ sinkDoc XML.def
-  let cursor = fromDocument document
-  liftIO $ cursor !<//=> [ "artist" ]
+  liftIO $ fromDocument document !<//=> docPath
 
 runMusicBrainz :: ResourceIO m => ResourceT (ReaderT Manager m) a -> m a
 runMusicBrainz actions = runResourceT $ do
